68eb41a15d64def2bcc9dc1ddd45fa61d5450729
Carles Gonzalvo <cgonzalvo@newrelic.com>
2023-05-27 15:03:07 +0200

    solution

--- client/index.js
+++ client/index.js
@@ -5,10 +5,19 @@ const MerkleTree = require('../utils/MerkleTree');
 const serverUrl = 'http://localhost:1225';
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // how do we prove to the server we're on the nice list?
+
+  // create a merkle tree for the whole list
+  const merkleTree = new MerkleTree(niceList);
+
+  // find the proof that norman block is in the list
+  const name = process.argv.slice(2).join(' ');
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
+  console.log(`proof: ${JSON.stringify(proof)}`);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name, proof
   });
 
   console.log({ gift });
